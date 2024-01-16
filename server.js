48d10b3e5ff4cf6529ef2a22c311d282fc01d23b
kcqua <kweku.quansah@ashesi.edu.gh>
2024-01-16 20:35:55 +0000

    first comit

--- server.js
+++ server.js
@@ -1,11 +1,31 @@
 const express =require('express')
 const app =express()
-
+const mongoose =require('mongoose')
 /// declare routh 
-
+app.use(express.json())
 app.get('/',(req,res)=>{
     res.send('Hello Node Api')
 })
-app.listen(3000,()=>{
+
+app.get('/post',(req,res)=>{
+    res.send('we love food')
+})
+
+app.post('/product',(req,res)=>{
+    console.log(req.body);
+    res.send(req.body)
+})
+mongoose.set('strictQuery',false)
+mongoose.connect('mongodb+srv://kcquansah:ashesi2023@userap.pczw6sm.mongodb.net/Node-Api?retryWrites=true&w=majority')
+.then(()=>
+{
+  
+    
+ console.log('Connected to MongoDB')   
+ app.listen(3500,()=>{
     console.log(`Node Api app is runnng on port 3000`)
+})
+}
+).catch(()=>{
+    console.log(eror)
 })
